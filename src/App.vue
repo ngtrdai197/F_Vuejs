93c67ef2d7c9df47a0b458637e303df1f23cc809
Nguyen Trong Dai <ngtrdai290197@gmail.com>
2019-02-10 01:22:46 +0700

    Giao tiếp giữa các component :tada: :tada: :boom:

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <HelloWorld :msg="movie_name" title="Say what ?" @clickedSomething="handleClickInParent"/>
+
+    <h2>
+      App-Component message:
+      <span v-if="checkType">Waiting for typing ...</span>
+      {{message}}
+    </h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
+  },
+  data: function() {
+    return {
+      message: "",
+      checkType: true,
+      movie_name: "Danh sách phim"
+    };
+  },
+  methods: {
+    handleClickInParent: function(value) {
+      this.message = value;
+      if (this.message !== "") {
+        this.checkType = !this.checkType;
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
